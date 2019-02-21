c63deb3ca440dcc31ddd01bb6be9f8a5850fe869
Rushi Shah <2016rshah@gmail.com>
2019-02-21 15:14:37 -0600

    Updates to assignment

--- hw2/print_results.py
+++ hw2/print_results.py
@@ -17,17 +17,20 @@ trace_list = open('sim_list/traces.txt', 'r')
 for trace in trace_list:
     trace = trace[:-1]
     trace_output = open('output/'+policy+'/'+trace+'.txt', 'r')
+    mpki = 0.0
+    ipc = 0.0
     for line in trace_output:
         if line.startswith('LLC TOTAL'):
             mpki = float(line.split()[7])/100000.0
-            mpki_list.append(mpki)
-            print('%.1f %s' % (mpki, trace))
+        elif line.startswith('LLC WRITEBACK'):
+            wb_miss = float(line.split()[7])/100000.0
+            mpki -= wb_miss
         elif line.startswith('CPU 0 cumulative IPC'):
             ipc = float(line.split()[4])
-            ipc_list.append(ipc)
-            print('%.3f' % (ipc), end=' ')
+    mpki_list.append(mpki)
+    ipc_list.append(ipc)
+    print('%.3f %2.1f %s' % (ipc, mpki, trace))
 
 avg_mpki = sum(mpki_list)/len(mpki_list)
 avg_ipc = sum(ipc_list)/len(ipc_list)
-print('%.3f %.1f Average' % (avg_ipc, avg_mpki))
-
+print('%.3f %2.1f Average' % (avg_ipc, avg_mpki))
